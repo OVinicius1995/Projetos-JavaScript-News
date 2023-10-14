9295a27cc46be546337aa477a2af0f0bf23b9c4b
Vinicius Oliveira <abreu-vinicius15@hotmail.com>
2023-10-13 21:32:00 -0300

    Correção da função de geração dinâmica.

--- funcoes.js
+++ funcoes.js
@@ -21,24 +21,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var divPost = document.createElement('div');
   divPost.className = 'post';
+  //divPost.innerHTML = 'Im here';
 
   var dateP = document.createElement('p');
   dateP.className = 'date';
+  dateP.innerHTML = '21 DE DEZEMBRO DE 2022';
 
-  document.createElement('br');
-
+  var postBr = document.createElement('br');
   var tituloPost = document.createElement('h1');
-  tituloPost.innerHTML = 'Título';
+  tituloPost.innerHTML = 'Primeira Publicação do Blog';
 
   var contentPost = document.createElement('div');
   contentPost.className = 'post-content';
-  
-  document.createElement('br');
-  document.createElement('p');
 
-  contentPost.innerHTML = 'Hi there I should be here';
-  //div.id = 'container';
- //div.innerHTML = 'Hi there!';
-  
-  document.getElementById('itenPost').appendChild(contentPost);
+  var conteudoSaltaLinha = document.createElement('br');
+  var conteudoParagrafo  = document.createElement('p');
+
+  conteudoParagrafo.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur rem qui nam veniam numquam maiores tempore quas vero, rerum vel? Nemo repellat sapiente nulla eos impedit veniam, natus dolores!';
+
+
+  document.getElementById('itenPost').appendChild(divPostContainer);
+  divPostContainer.appendChild(divPost);
+  divPost.appendChild(dateP);
+  divPost.appendChild(postBr);
+  divPost.appendChild(tituloPost);
+  divPost.appendChild(contentPost);
+  contentPost.appendChild(conteudoSaltaLinha);
+  contentPost.appendChild(conteudoParagrafo);
 }, false);
