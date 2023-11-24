c5ab2c7c74f167bb98d10459e2e7df80c5f44635
Vinicius Oliveira <abreu-vinicius15@hotmail.com>
2023-11-24 19:22:43 -0300

    Iniciando back-end de inserseção de dados indicações

---
+++ funcoesIndicacoes.js
@@ -0,0 +1,24 @@
+const openModal           = document.getElementById('showModal');
+const openModalUpdateNews = document.getElementById('showModalCorrigir');
+
+
+function openModalNew(){
+  openModal.classList.toggle('active');
+}
+
+function closeModalNew(){
+  openModal.classList.remove('active');
+}
+
+function openModalUpdate(){
+  openModalUpdateNews.classList.toggle('active');
+}
+
+function closeModalUpdate(){
+  openModalUpdateNews.classList.remove('active');
+}
+
+function search(){
+
+  
+}
